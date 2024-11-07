efe0382d302252e65d2c290edcc87299f19c4333
kavya <kavyashree@maitravaruna.com>
2024-11-07 09:40:10 +0530

    Update mpd_v1.sv

--- kavya_SV/mpd_v1.sv
+++ kavya_SV/mpd_v1.sv
@@ -1,23 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/06/2024 10:25:04 AM
-// Design Name: 
-// Module Name: mpdmk
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/////////
 
 
 `timescale 1ns / 1ps
